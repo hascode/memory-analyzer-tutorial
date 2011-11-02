78fc13b55a84959d5577135b40fcc29d5642cfe6
hascode <hascode@localhost>
2011-11-02 21:40:31 +0100

    Initial import

---
+++ src/com/hascode/tutorial/Main.java
@@ -0,0 +1,15 @@
+package com.hascode.tutorial;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+	public static void main(final String... args) {
+		System.out.println("start");
+		List<Integer> numbers = new ArrayList<Integer>();
+		for (int i = 0; i < Integer.MAX_VALUE; i++) { // going to fail
+			numbers.add(i);
+		}
+		System.out.println("stop");
+	}
+}
