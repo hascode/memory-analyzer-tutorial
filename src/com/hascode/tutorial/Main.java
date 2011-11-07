141e42989c410b13a048bac51cc55547273967e6
hascode <hascode@localhost>
2011-11-07 19:48:33 +0100

    Typo in loop fixed

--- src/com/hascode/tutorial/Main.java
+++ src/com/hascode/tutorial/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(final String... args) {
 		System.out.println("start");
 		List<Integer> numbers = new ArrayList<Integer>();
-		for (int i = 0; i < Integer.MAX_VALUE; i++) { // going to fail
+		for (int i = 0; i <= Integer.MAX_VALUE; i++) { // going to fail
 			numbers.add(i);
 		}
 		System.out.println("stop");
